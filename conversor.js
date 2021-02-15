9efd2d1d5bae1155a89a81191a3d4e36a6712640
hvilela0 <39452510+hvilela0@users.noreply.github.com>
2021-02-15 17:54:16 -0300

    ';' adds at some lines

--- conversor.js
+++ conversor.js
@@ -4,7 +4,7 @@
  * @version 0.1
  */
 
-const fs = require('fs');;
+const fs = require('fs');
 
 //get arguments on running (the HTML filename with extension)
 filename = process.argv[2];
@@ -16,16 +16,16 @@ fs.readFile(`./${filename}`, 'utf-8', function (err, data) {
     let findQuotes = /"/g;
     let findLineBreaks = /\r\n/g;
 
-    data = data.replace(findQuotes, '\\"')
-    data = data.replace(findLineBreaks, '";\nmsg+= "')
+    data = data.replace(findQuotes, '\\"');
+    data = data.replace(findLineBreaks, '";\nmsg+= "');
 
     // start and end of file adjustments
-    data = data.replace('<!DOCTYPE html>', 'String msg;\n\nmsg+= "<!DOCTYPE html>')
-    data = data.replace('</html>', '</html>";')
+    data = data.replace('<!DOCTYPE html>', 'String msg;\n\nmsg+= "<!DOCTYPE html>');
+    data = data.replace('</html>', '</html>";');
 
     //write out the file to copy and past on the C/C++ file
     fs.writeFile('./out.txt', data, 'utf-8', function (err) {
         if (err) throw err;
-    })
+    });
 
-})
+});
